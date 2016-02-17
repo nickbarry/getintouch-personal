4837f165673fe18c336e4564598f48e32050da20
Nick Barry <niko.barry@gmail.com>
2016-02-17 10:53:06 -0800

    add Routes file

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,14 @@
 /**
  * Created by Nicholas Barry on 2/17/2016.
  */
+var express = require('express'),
+    router = express.Router(),
+    home = require('../controllers/home'),// todo: haven't created yet
+    contact = require('../controllers/contact');// todo: haven't created yet
+
+module.exports = function(app){
+    router.get('/', home.index);// todo: haven't created yet
+    router.get('/contact/:contact_id', contact.index);// todo: haven't created yet
+
+    app.use(router);
+};
