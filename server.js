3685ad3c53dd5a8f56e2e14c4f16213e346b2e85
Nick Barry <niko.barry@gmail.com>
2016-02-17 12:03:39 -0800

    add Mongoose

--- server.js
+++ server.js
@@ -3,6 +3,18 @@
  */
 var express = require('express'),
     app = express(),
-    config = require('./server/configure');
+    config = require('./server/configure'),
+    mongoose = require('mongoose');
 
 app = config(app);
+
+mongoose.connect('mongodb://localhost/getintouch');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', function(){
+    console.log('Mongoose connected.');
+});
+
+var server = app.listen(app.get('port'), function(){
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
