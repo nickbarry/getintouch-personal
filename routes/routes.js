d878e68c09608df6505b699641400f331f3ad7a5
Nick Barry <niko.barry@gmail.com>
2016-02-21 10:16:05 -0800

    misc minor updates

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,16 @@
 /**
  * Created by Nicholas Barry on 2/17/2016.
  */
-var express = require('express'),
-    router = express.Router(),
-    home = require('../controllers/home'),// todo: haven't created yet
-    contact = require('../controllers/contact');// todo: haven't created yet
+var express = require('express');
+var router = express.Router();
+//var home = require('../controllers/home');// todo: haven't created yet
+//var contact = require('../controllers/contact');// todo: haven't created yet
 
-router.get('/', home.index);// todo: haven't created yet
-router.get('/contact/:contact_id', contact.index);// todo: haven't created yet
+router.get('/', function(req,res){
+    res.send('Hello world!')
+});
+router.get('/contact/:contact_id', function(req,res){
+    res.send('Hello world!')
+});
 
 module.exports = router;
