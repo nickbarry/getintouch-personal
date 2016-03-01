dcde7432df6533dc68479ee1f233cfe35154ae4a
Nick Barry <niko.barry@gmail.com>
2016-02-29 18:10:52 -0800

    App middleware set-up per textbook: https://www.packtpub.com/packtlib/book/Web%20Development/9781783981083/1/ch01lvl1sec12/The%20application%20structure

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var morgan = require('morgan');
+var flash = require('connect-flash');
 var path = require('path');
 var methodOverride = require('method-override');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var moment = require('moment');
+var errorHandler = require('errorhandler');
+var config = require('./configUntracked');
+var db = require('./lib/db');
 
 var router = require('./routes/routes');
 
@@ -27,12 +32,26 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride());
+app.use(methodOverride(function(req, res){
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in url - encoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+app.use(cookieSession({
+    secret: config.sessionSecret,
+    cookie: {
+        maxAge: config.sessionMaxAge
+    }
+}));
+app.use(flash());
 app.use(router);
 
 // catch 404 and forward to error handler
