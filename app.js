520666b93211a25123d79485572d905a9ac0d15b
Nick Barry <niko.barry@gmail.com>
2016-03-19 20:27:23 -0700

    Fix error with static middleware I'm not sure why it was working before I made this change, but I believe this is the correct syntax. I had left off the '\' before public in the path, meaning that I was searching for 'C:\apps\getintouch-personalpublic' instead of 'C:\apps\getintouch-personal\public'. I noticed it because after I closed and reopened my project, it stopped working.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '\\public'));
 app.use(methodOverride(function(req, res){
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // TODO: For the corresponding html, see the textbook note
