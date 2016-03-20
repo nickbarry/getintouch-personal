0e58d2c45b165f12d727da080c36bc4d7f7e19d1
Nick Barry <niko.barry@gmail.com>
2016-03-19 18:31:38 -0700

    [WIP] Add show/hide functionality to "Show Details" links Add a scripts.js file, jquery, and some new classes and IDs on the index page to allow the user to show/hide the details section of each contact card by clicking the "Show/Hide details" link.

---
+++ public/javascript/scripts.js
@@ -0,0 +1,13 @@
+$(
+    $('#contact-cards-container').on('click', '.show-hide-details', function(event){
+        event.preventDefault();
+        var link = event.target; // Get link element that was clicked
+        var details = link.closest('.panel-body').find('.details');
+        details.toggleClass('hidden');
+        if(link.text() === "Show details"){ // If details were hidden previously
+            link.text('Hide details');
+        }else{
+            link.text('Show details');
+        }
+    });
+);
