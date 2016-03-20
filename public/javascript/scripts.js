45415179ac14d97185cc320aaeda865c8fe7a39c
Nick Barry <niko.barry@gmail.com>
2016-03-19 19:09:40 -0700

    Implement show/hide for contact card details Fix last few errors to get the show/hide functionality working:  - Included scripts.js file in 'main' layout  - Fix error with my document.ready() syntax  - Fix error with jquery handling of elements (link needed to be wrapped in $ before I could use jquery functions on it)

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,13 +1,15 @@
-$(
-    $('#contact-cards-container').on('click', '.show-hide-details', function(event){
-        event.preventDefault();
-        var link = event.target; // Get link element that was clicked
-        var details = link.closest('.panel-body').find('.details');
-        details.toggleClass('hidden');
-        if(link.text() === "Show details"){ // If details were hidden previously
-            link.text('Hide details');
-        }else{
-            link.text('Show details');
-        }
-    });
-);
+$(function(){
+    $('#contact-cards-container').on('click', '.show-hide-details', showOrHideDetails);
+});
+
+function showOrHideDetails(event){
+    event.preventDefault();
+    var $link = $(event.target); // Get link element that was clicked
+    var details = $link.closest('.panel-body').find('.details');
+    details.toggleClass('hidden');
+    if($link.text() === "Show details"){ // If details were hidden previously
+        $link.text('Hide details');
+    }else{
+        $link.text('Show details');
+    }
+}
