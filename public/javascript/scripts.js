8d75ac298da292dbcbc78f724157677c94133e8b
Nick Barry <niko.barry@gmail.com>
2016-04-01 09:52:02 -0700

    [WIP] Ability to update a contact: Done in this commit: Add ability for scripts.js file to discern between pages so it can invoke relevant functions Add some (incomplete) functionality for buttons on the individual contact page
    To do:
    Set up url to change between ‘/edit’ and no ‘/edit’
    Set hidden/visible content based on whether ‘/edit’ is in url
    Find out how to programmatically submit form, as a PUT rather than a Post
    What happens after form submission? Can I prevent page from reloading/redirecting automatically? Should I use jquery to update the contents of the details elements, or just reload the page? Maybe start with the latter, and then consider the former afterward.
    Formatting: Make the spacing of the fields even between edit and details views
    Formatting: TODO: get rid of this separate line, and make the nameFull field below, in the form, larger. Model after Gmail Contacts
    Maybe improve my url-detecting logic in scripts.js by splitting url according to slashes. Note that some things should work across pages, even if they may exist in different contexts. A contact Delete button should probably always do the same thing, and I should probably just trust that I’ll only put it where it should be clickable.

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,15 +1,53 @@
-$(function(){
-    $('#contact-cards-container').on('click', '.show-hide-details', showOrHideDetails);
+$(function(){ // on document ready
+    var baseUrl = 'http://localhost:3000',// TODO: Need to have this automatically based on dev or other environment
+        url = window.location.href, // Get current page URL
+        urlRemainder = url.replace(baseUrl,'');
+
+    $('#c-contact-cards-container').on('click', '.show-hide-details', showOrHideDetails); // Toggles contact card details
+
+    if(/^\/?contact\//.test(urlRemainder)){ // If user is on contact page, or contact/edit page
+        $('#js-contact-edit-button').on('click',editContactDetails);
+        $('#js-contact-update-button').on('click',saveContactDetails);
+        $('#js-contact-update-cancel-button').on('click',cancelEditContactDetails);
+    }
+
+    // if(/^\/?$/.test(urlRemainder)){} // User is on homepage, i.e. urlRemainder is '' or '/'
 });
 
+// FUNCTIONS FOR PAGES DISPLAYING CONTACT CARDS ------------------------------------------------------------------------
+// Toggle the visibility of the Notes field
 function showOrHideDetails(event){
     event.preventDefault();
     var $link = $(event.target); // Get link element that was clicked
-    var details = $link.closest('.panel-body').find('.details');
-    details.toggleClass('hidden');
+    var $details = $link.closest('.panel-body').find('.details');
+    $details.toggleClass('hidden');
     if($link.text() === "Show details"){ // If details were hidden previously
         $link.text('Hide details');
     }else{
         $link.text('Show details');
     }
+}
+
+// CONTACT DETAILS PAGE FUNCTIONS --------------------------------------------------------------------------------------
+// Edit contact details
+function editContactDetails(event){
+    event.preventDefault();
+    toggleEditAndDetailViews();
+}
+
+// Cancel editing contact details
+function cancelEditContactDetails(event){
+    event.preventDefault();
+    toggleEditAndDetailViews();
+}
+
+// Save details from contact form
+function saveContactDetails(event){
+    event.preventDefault();
+    toggleEditAndDetailViews();
+}
+
+// Toggle visibility of edit and view details
+function toggleEditAndDetailViews(){
+    $('.js-details-view, .js-edit-view').toggleClass('hidden');
 }
