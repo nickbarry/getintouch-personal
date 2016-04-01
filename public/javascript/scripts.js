675ec5160cfd1f98df3343aaafec4266b48c0aa7
Nick Barry <niko.barry@gmail.com>
2016-04-01 10:43:08 -0700

    [WIP] Ability to update a contact: Done in this commit: Change default visibility of details/edit elements - default view should be Details view, with input (edit) elements hidden initially. Improve my url-detecting logic in scripts.js by splitting url according to slashes Set hidden/visible content based on whether ‘/edit’ is in url Set up url to change between ‘/edit’ and no ‘/edit’
    To do:
    Pass some page state information in pushState method, and add an event listener so the page can show the correct content if the user presses Back
    Programmatically submit form, as a PUT rather than a Post
    What happens after form submission? Can I prevent page from reloading/redirecting automatically? Should I use jquery to update the contents of the details elements, or just reload the page? Maybe start with the latter, and then consider the former afterward.
    Formatting: Make the spacing of the fields even between edit and details views
    Formatting: get rid of this separate line, and make the nameFull field below, in the form, larger. Model after Gmail Contacts
    Any other TODOs in the js or html of these pages: scripts.js, contact.handlebars

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,17 +1,14 @@
 $(function(){ // on document ready
-    var baseUrl = 'http://localhost:3000',// TODO: Need to have this automatically based on dev or other environment
-        url = window.location.href, // Get current page URL
-        urlRemainder = url.replace(baseUrl,'');
+    var urlArr = window.location.href.split('/'); // Split current page URL up into an array, e.g. ['http:', '', 'www.getintou.ch', 'contact', 'noch3no83hni8onc8']
 
     $('#c-contact-cards-container').on('click', '.show-hide-details', showOrHideDetails); // Toggles contact card details
 
-    if(/^\/?contact\//.test(urlRemainder)){ // If user is on contact page, or contact/edit page
+    if('contact' === urlArr[3]){ // If user is on contact/[contact_id] page, or contact/[contact_id]/edit page
+        if('edit' === urlArr[5]) toggleEditAndDetailViews(); // If on edit page, reveal edit view
         $('#js-contact-edit-button').on('click',editContactDetails);
         $('#js-contact-update-button').on('click',saveContactDetails);
         $('#js-contact-update-cancel-button').on('click',cancelEditContactDetails);
     }
-
-    // if(/^\/?$/.test(urlRemainder)){} // User is on homepage, i.e. urlRemainder is '' or '/'
 });
 
 // FUNCTIONS FOR PAGES DISPLAYING CONTACT CARDS ------------------------------------------------------------------------
@@ -31,23 +28,38 @@ function showOrHideDetails(event){
 // CONTACT DETAILS PAGE FUNCTIONS --------------------------------------------------------------------------------------
 // Edit contact details
 function editContactDetails(event){
-    event.preventDefault();
+    event.preventDefault(); // TODO: Get rid of these once I move buttons out of form, right?
     toggleEditAndDetailViews();
+    toggleEditUrl();
 }
 
 // Cancel editing contact details
 function cancelEditContactDetails(event){
     event.preventDefault();
     toggleEditAndDetailViews();
+    toggleEditUrl();
 }
 
 // Save details from contact form
 function saveContactDetails(event){
     event.preventDefault();
     toggleEditAndDetailViews();
+    toggleEditUrl();
 }
 
 // Toggle visibility of edit and view details
 function toggleEditAndDetailViews(){
     $('.js-details-view, .js-edit-view').toggleClass('hidden');
+}
+
+// Toggle '/edit' in current url, without reloading the page
+function toggleEditUrl(){
+    var urlArr = window.location.href.split('/'); // Split current page URL up into an array, e.g. ['http:', '', 'www.getintou.ch', 'contact', 'noch3no83hni8onc8']
+    if('edit' === urlArr[urlArr.length-1]){ // If user is on the 'edit' version of a page
+        urlArr.length--; // Delete the 'edit' from the urlArr
+        history.pushState(null, $('#js-nameFull').text(), urlArr.join('/')); // TODO: Can I do a relative link that just goes up a directory, like '/..'?
+    }else{
+        urlArr[urlArr.length] = 'edit'; // Add 'edit' to the urlArr
+        history.pushState(null, $('#js-nameFull').text() + ' - Edit', urlArr.join('/')); // TODO: Can I do a relative link that just goes up a directory, like '/..'?
+    }
 }
